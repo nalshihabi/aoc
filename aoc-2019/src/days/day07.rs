1b55a8559a34b203093e648e8e42e7f918539834
Noah AL-Shihabi <noahshihabi@gmail.com>
2019-12-07 13:50:57 -0500

    Rewrite intcode vm

--- aoc-2019/src/days/day07.rs
+++ aoc-2019/src/days/day07.rs
@@ -18,11 +18,13 @@ fn read_input() -> Vec<i32> {
         .collect()
 }
 
+type Instruction = (i32, i32, i32, i32);
 struct Vm {
     index: usize,
     program: Vec<i32>,
-    output: Vec<i32>,
     running: bool,
+    display: bool,
+    debug_display: bool,
 }
 
 impl Vm {
@@ -30,12 +32,13 @@ impl Vm {
         Vm {
             index: 0,
             program: program,
-            output: Vec::new(),
             running: true,
+            display: false,
+            debug_display: false,
         }
     }
 
-    fn det_instruction(&self, mut num: i32) -> (i32, i32, i32, i32) {
+    fn parse_instruction(&self, mut num: i32) -> Instruction {
         let de = num % 100;
         num /= 100;
         let c = num % 10;
@@ -46,12 +49,12 @@ impl Vm {
         (a, b, c, de)
     }
 
-    fn det_values(&self, inst: (i32, i32, i32, i32)) -> (i32, i32, i32) {
+    fn read_parameters(&self, inst: Instruction) -> (i32, i32, usize) {
         let index = self.index;
         let inp = &self.program;
         let p1: i32;
         let p2: i32;
-        let de: i32;
+        let de: usize;
 
         if inst.2 == 0 {
             p1 = inp[inp[index + 1] as usize];
@@ -66,9 +69,9 @@ impl Vm {
         }
 
         if inst.0 == 0 {
-            de = inp[index + 3];
+            de = inp[index + 3] as usize;
         } else {
-            de = inp[inp[index + 3] as usize];
+            de = inp[inp[index + 3] as usize] as usize;
         }
 
         (p1, p2, de)
@@ -86,111 +89,184 @@ impl Vm {
         }
     }
 
-    fn step(&mut self, input: Option<i32>) -> Option<i32> {
-        let op = self.program[self.index];
-        if op == 99 {
-            self.running = false;
-            return None;
+    fn add(&mut self, inst: Instruction) -> Option<i32> {
+        let (p1, p2, de) = self.read_parameters(inst);
+        self.program[de] = p1 + p2;
+        self.index += 4;
+        None
+    }
+
+    fn mul(&mut self, inst: Instruction) -> Option<i32> {
+        let (p1, p2, de) = self.read_parameters(inst);
+        self.program[de] = p1 * p2;
+        self.index += 4;
+        None
+    }
+
+    fn read(&mut self, input: Option<i32>) -> Option<i32> {
+        let p1: i32;
+        let de = self.program[self.index + 1] as usize;
+
+        if !input.is_some() {
+            let mut input = String::new();
+            match io::stdin().read_line(&mut input) {
+                Ok(_n) => {
+                    p1 = input.trim().parse().expect("Not an integer");
+                }
+                Err(_error) => p1 = 0,
+            };
+        } else {
+            p1 = input.clone().expect("No text input");
         }
 
-        let inst = self.det_instruction(op);
-        let p1: i32;
-        let p2: i32;
-        let de: i32;
+        self.program[de] = p1;
+        self.index += 2;
+        None
+    }
 
-        if inst.3 == 1 {
-            let vals = self.det_values(inst);
-            p1 = vals.0;
-            p2 = vals.1;
-            de = vals.2;
+    fn write(&mut self) -> Option<i32> {
+        let de = self.program[self.index + 1] as usize;
+        if self.display {
+            println!("{}", self.program[de]);
+        }
 
-            self.program[de as usize] = p1 + p2;
-            self.index += 4;
-        } else if inst.3 == 2 {
-            let vals = self.det_values(inst);
-            p1 = vals.0;
-            p2 = vals.1;
-            de = vals.2;
+        self.index += 2;
+        Some(self.program[de])
+    }
 
-            self.program[de as usize] = p1 * p2;
-            self.index += 4;
-        } else if inst.3 == 3 {
-            p1 = self.program[self.index + 1];
-
-            if !input.is_some() {
-                let mut input = String::new();
-                match io::stdin().read_line(&mut input) {
-                    Ok(_n) => {
-                        de = input
-                            .as_str()
-                            .trim()
-                            .parse::<i32>()
-                            .expect("Not an integer");
-                    }
-                    Err(_error) => de = 0,
-                };
-            } else {
-                de = input.clone().expect("No text input");
-            }
-
-            self.program[p1 as usize] = de;
-            self.index += 2;
-        } else if inst.3 == 4 {
-            p1 = self.program[self.index + 1];
-            println!("{}", self.program[p1 as usize]);
-            self.output.push(self.program[p1 as usize]);
-            self.index += 2;
-            return Some(self.program[p1 as usize]);
-        } else if inst.3 == 5 {
-            let vals = self.det_values(inst);
-            p1 = vals.0;
-            p2 = vals.1;
-
-            if p1 != 0 {
-                self.index = p2 as usize;
-            } else {
-                self.index += 3;
-            }
-        } else if inst.3 == 6 {
-            let vals = self.det_values(inst);
-            p1 = vals.0;
-            p2 = vals.1;
-
-            if p1 == 0 {
-                self.index = p2 as usize;
-            } else {
-                self.index += 3;
-            }
-        } else if inst.3 == 7 {
-            let vals = self.det_values(inst);
-            p1 = vals.0;
-            p2 = vals.1;
-            de = vals.2;
-
-            if p1 < p2 {
-                self.program[de as usize] = 1;
-            } else {
-                self.program[de as usize] = 0;
-            }
-
-            self.index += 4;
-        } else if inst.3 == 8 {
-            let vals = self.det_values(inst);
-            p1 = vals.0;
-            p2 = vals.1;
-            de = vals.2;
-
-            if p1 == p2 {
-                self.program[de as usize] = 1;
-            } else {
-                self.program[de as usize] = 0;
-            }
-
-            self.index += 4;
+    fn jne(&mut self, inst: Instruction) -> Option<i32> {
+        let (p1, p2, _de) = self.read_parameters(inst);
+        if p1 != 0 {
+            self.index = p2 as usize;
+        } else {
+            self.index += 3;
         }
 
         None
     }
+
+    fn je(&mut self, inst: Instruction) -> Option<i32> {
+        let (p1, p2, _de) = self.read_parameters(inst);
+        if p1 == 0 {
+            self.index = p2 as usize;
+        } else {
+            self.index += 3;
+        }
+
+        None
+    }
+
+    fn cmpl(&mut self, inst: Instruction) -> Option<i32> {
+        let (p1, p2, de) = self.read_parameters(inst);
+
+        if p1 < p2 {
+            self.program[de] = 1;
+        } else {
+            self.program[de] = 0;
+        }
+
+        self.index += 4;
+        None
+    }
+
+    fn cmpe(&mut self, inst: Instruction) -> Option<i32> {
+        let (p1, p2, de) = self.read_parameters(inst);
+
+        if p1 == p2 {
+            self.program[de] = 1;
+        } else {
+            self.program[de] = 0;
+        }
+
+        self.index += 4;
+        None
+    }
+
+    fn step(&mut self, input: Option<i32>) -> Option<i32> {
+        let inst = self.parse_instruction(self.program[self.index]);
+        if self.debug_display {
+            println!(
+                "At {} for {:?}\n  Program: {:?}",
+                self.index, inst, self.program
+            );
+        }
+
+        match inst.3 {
+            99 => {
+                self.running = false;
+                None
+            }
+            1 => self.add(inst),
+            2 => self.mul(inst),
+            3 => self.read(input),
+            4 => self.write(),
+            5 => self.jne(inst),
+            6 => self.je(inst),
+            7 => self.cmpl(inst),
+            8 => self.cmpe(inst),
+            _ => None,
+        }
+    }
+}
+
+fn part1_emulate(items: &Vec<i32>, program: &Vec<i32>, best: &mut Vec<(i32, Vec<i32>)>) {
+    let mut output = 0;
+
+    items.iter().for_each(|item| {
+        let mut vm = Vm::new(program.clone());
+        vm.step_until_input();
+        vm.step(Some(*item));
+        vm.step_until_input();
+        vm.step(Some(output));
+        vm.step_until_print();
+        let cur_output = vm.step(None);
+
+        if cur_output.is_none() {
+            return;
+        } else {
+            output = cur_output.expect("There should be output here");
+        }
+    });
+
+    best.push((output, items.clone()));
+}
+
+fn part2_emulate(items: &Vec<i32>, program: &Vec<i32>, best: &mut Vec<(i32, Vec<i32>)>) {
+    let mut vms: Vec<Vm> = items.iter().map(|_item| Vm::new(program.clone())).collect();
+    let mut inputs: Vec<i32> = vec![0; items.len()];
+    let mut cur_best = 0;
+    let mut first_done = false;
+
+    while !vms
+        .iter()
+        .fold(false, |one_done, vm| one_done || !vm.running)
+    {
+        for i in 0..vms.len() {
+            if !first_done {
+                vms[i].step_until_input();
+                vms[i].step(Some(items[i]));
+            }
+            vms[i].step_until_input();
+            vms[i].step(Some(inputs[i]));
+            vms[i].step_until_print();
+
+            let cur_output = vms[i].step(None);
+            if cur_output.is_none() {
+                break;
+            } else {
+                let output = cur_output.expect("There should be output");
+                let next = (i + 1) % inputs.len();
+                inputs[next] = output;
+
+                if output > cur_best {
+                    cur_best = output;
+                }
+            }
+        }
+
+        first_done = true;
+    }
+    best.push((cur_best, items.clone()));
 }
 
 fn recur(
@@ -204,62 +280,9 @@ fn recur(
 ) {
     if loc == items.len() {
         if part1 {
-            let mut vm = Vm::new(inp.clone());
-            let mut output = 0;
-            items.iter().for_each(|item| {
-                vm.step_until_input();
-                vm.step(Some(*item));
-                vm.step_until_input();
-                vm.step(Some(output));
-                vm.step_until_print();
-                let cur_output = vm.step(None);
-
-                if cur_output.is_none() {
-                    return;
-                } else {
-                    output = cur_output.expect("There should be output here");
-                }
-            });
-            best.push((output, items.clone()));
+            part1_emulate(items, inp, best);
         } else {
-            let mut vms: Vec<Vm> = items.iter().map(|_item| Vm::new(inp.clone())).collect();
-
-            let mut inputs: Vec<i32> = vec![0; items.len()];
-            let mut cur_best = 0;
-            let mut first_done = false;
-
-            while !vms
-                .iter()
-                .fold(false, |one_done, vm| one_done || !vm.running)
-            {
-                for i in 0..vms.len() {
-                    if !first_done {
-                        vms[i].step_until_input();
-                        vms[i].step(Some(items[i]));
-                        // first_done = true;
-                    }
-                    vms[i].step_until_input();
-                    vms[i].step(Some(inputs[i]));
-                    vms[i].step_until_print();
-
-                    let cur_output = vms[i].step(None);
-                    if cur_output.is_none() {
-                        break;
-                    } else {
-                        let output = cur_output.expect("There should be output");
-                        let next = (i + 1) % inputs.len();
-                        inputs[next] = output;
-
-                        if output > cur_best {
-                            cur_best = output;
-                        }
-                    }
-                }
-
-                first_done = true;
-            }
-
-            best.push((cur_best, items.clone()));
+            part2_emulate(items, inp, best);
         }
     } else {
         for i in 0..items.len() {
